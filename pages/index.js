736509f310470aaa5878fc910bcd61234745bd6c
robin828 <robin19093@iiitd.ac.in>
2023-06-14 03:11:22 +0530

    routing done

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 import './styles.css'
 function index() {
+    const router = useRouter();
+    const handleRedirect = () => {
+        router.push('/App');
+      };
   return (
+    
     <div className='h-[100vh] w-full bg-pageBg flex flex-col justify-center' >
     <div className='text-[#333333] justify-center flex  gap-[30px] w-full' >
-    <button className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
-        Chart 1
+    <button onClick={()=>router.push('/App')} className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
+        Phase 1
     </button>
-    <button className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
-        Chart 1
+    <button onClick={()=>router.push('/LineLiveChart')} className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
+        Phase 2
     </button>
-    <button className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
-        Chart 1
+    <button onClick={()=>router.push('/CandleLiveChart')} className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
+        Phase 3
     </button>
-    <button className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
-        Chart 1
+    <button onClick={()=>router.push('/MultipleInstrument')} className='p-3 bg-white rounded-lg hover:scale-105  shadow-red-200'>
+        Phase 4
     </button>
 </div>
 </div>
